7a54f57bb4feaca09b8237901c44f57cfa900ec1
vanhanenjjv <janne.vanhanen@outlook.com>
2021-07-03 22:04:47 +0000

    Deploying to gh-pages from @ Kuisti/Website@9c484fb4195c4dc5db0a791df1c7d10f604e5d95 🚀

---
+++ precache-manifest.144b73afeb01724bf4f3f9e152c85a94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d240d6bf306ca67f546e2aa53c02f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2d91578459354ecbb8d",
+    "url": "/static/css/main.104c0c26.chunk.css"
+  },
+  {
+    "revision": "da6cb24ad9049afd35db",
+    "url": "/static/js/2.3c250df9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3c250df9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2d91578459354ecbb8d",
+    "url": "/static/js/main.7a9f7c4e.chunk.js"
+  },
+  {
+    "revision": "21ee48b7c28727f88e0f",
+    "url": "/static/js/runtime-main.d7347b0e.js"
+  },
+  {
+    "revision": "6234025e43c1802d6eec846ddb38b30f",
+    "url": "/static/media/kuisti.6234025e.glb"
+  },
+  {
+    "revision": "b8ac4353e4516b6238f4c7f073e150c1",
+    "url": "/static/media/kuisti.b8ac4353.png"
+  }
+]);
